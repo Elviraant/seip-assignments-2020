4967ee57be1997f4af474bf5c951551750ddfa4d
Elviraant <t8170007@dias.aueb.gr>
2020-04-20 16:03:37 +0300

    Create isPrime method in MyMath.java

--- unittesting/src/main/java/math/MyMath.java
+++ unittesting/src/main/java/math/MyMath.java
@@ -1,10 +1,19 @@
 package math;
 
+/**
+* The MyMath class contains more complex
+* mathematic operations to practice on unit testing
+* @author  Elviraant
+* @since   2020-04-17 
+*/
 public class MyMath {
 
 	/*
-	 * This is a method that calculates the 
-	 * factorial for every n between 0 and 12
+	 * Calculates and returns the factorial 
+	 * of an integer between 0 and 12
+	 * @param an integer
+	 * @return an integer
+	 * @exception IllegalArgumentException when the given integer is less than 0 or greater than 12
 	 */
 	public int factorial(int n) {
 		if (n < 0 || n > 12) {
@@ -14,5 +23,28 @@ public class MyMath {
 		} else {
 			return n*factorial(n-1);
 		}
+	}
+	
+	/*
+	 * Calculates whether an integer
+	 * greater than 2 is prime, 
+	 * returns true, if it is 
+	 * and false, otherwise
+	 * @param an integer
+	 * @return true if the input is prime, false otherwise
+	 * @exception IllegalArgumentException when the given integer is less than 2
+	 */
+	public boolean isPrime(int n) {
+		if (n < 2) {
+			throw new IllegalArgumentException("n should be >= 2");
+		} else {
+			for (int i = 2; i <= 9; i++) {
+				if (i < n & n%i == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+		
 	}
 }
