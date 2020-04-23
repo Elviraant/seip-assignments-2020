6a2d1611a3eb534548fb78af0ea369feff472d63
Elviraant <t8170007@dias.aueb.gr>
2020-04-23 15:28:11 +0300

    Fix test cases for the findPrimesInFile method
    Mock the MyMath class
    Remove unnecessary test cases

--- unittesting/src/test/java/math/ArrayOperationsTest.java
+++ unittesting/src/test/java/math/ArrayOperationsTest.java
@@ -14,28 +14,6 @@ public class ArrayOperationsTest {
 	/*
 	 * A test case for FindPrimesInFile
 	 * with a file input
-	 * that doesn't contain 
-	 * any primes
-	 */
-	@Test 
-	public void testFindPrimesInFileNoPrimes() {
-		
-		FileIO fileIO = mock(FileIO.class);
-		MyMath myMath = new MyMath();
-		
-		String filepath = "src/test/resources/noprimes.txt";
-		
-		when(fileIO.readFile(filepath)).thenReturn( new int[] { 4, 6, 15, 27, 82, 99});
-		
-		int[] expectedValues = new int[] {};
-		int[] actual = ao.findPrimesInFile(fileIO, filepath, myMath);
-		
-		Assert.assertArrayEquals(expectedValues, actual);
-	}
-	
-	/*
-	 * A test case for FindPrimesInFile
-	 * with a file input
 	 * that contains both 
 	 * primes and composites
 	 */
@@ -43,40 +21,24 @@ public class ArrayOperationsTest {
 	public void testFindPrimesInFilesNormal() {
 		
 		FileIO fileIO = mock(FileIO.class);
-		MyMath myMath = new MyMath();
+		MyMath myMath = mock(MyMath.class);
 		
 		String filepath = "src/test/resources/normal.txt";
 		
 		when(fileIO.readFile(filepath)).thenReturn( new int[] { 4, 2, 15, 27, 89, 97});
-		
+		when(myMath.isPrime(4)).thenReturn(false);
+		when(myMath.isPrime(2)).thenReturn(true);
+		when(myMath.isPrime(15)).thenReturn(false);
+		when(myMath.isPrime(27)).thenReturn(false);
+		when(myMath.isPrime(89)).thenReturn(true);
+		when(myMath.isPrime(97)).thenReturn(true);
+				
 		int[] expectedValues = new int[] { 2, 89, 97};
 		int[] actual = ao.findPrimesInFile(fileIO, filepath, myMath);
 		
 		Assert.assertArrayEquals(expectedValues, actual);
 	}
 	
-	
-	/*
-	 * A test case for FindPrimesInFile
-	 * with a file input
-	 * that only contains 
-	 * prime numbers
-	 */
-	@Test 
-	public void testFindPrimesInFilesOnlyPrimes() {
-		
-		FileIO fileIO = mock(FileIO.class);
-		MyMath myMath = new MyMath();
-		
-		String filepath = "src/test/resources/primes.txt";
-		
-		when(fileIO.readFile(filepath)).thenReturn( new int[] { 103, 2, 17, 97, 41, 89});
-		
-		int[] expectedValues = new int[] { 103, 2, 17, 97, 41, 89};
-		int[] actual = ao.findPrimesInFile(fileIO, filepath, myMath);
-		
-		Assert.assertArrayEquals(expectedValues, actual);
-	}
 	
 	
 	
